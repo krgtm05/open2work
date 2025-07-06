504476a3ba90f21af386a5f51f7a74e8893a7e87
krgtm05 <krgautamofficial@gmail.com>
2025-07-06 21:08:02 +0530

    Tailwind Configured & Started working on the Landing Page

--- Client/src/Components/RoleSelection/RoleSelection.jsx
+++ Client/src/Components/RoleSelection/RoleSelection.jsx
@@ -13,29 +13,27 @@ export default function RoleSelection() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        gap: "2rem",
-        marginTop: "4rem",
-      }}
-    >
-      <button
-        onClick={() => handleRoleSelect("employer")}
-        style={{ padding: "1rem 2rem" }}
-      >
-        I am an Employer
-      </button>
-      <button
-        onClick={() => handleRoleSelect("candidate")}
-        style={{ padding: "1rem 2rem" }}
-      >
-        I am a Candidate
-      </button>
-      <button onClick={() => handleLogin()} style={{ padding: "1rem 2rem" }}>
-        Login
-      </button>
+    <div className='container h-screen flex justify-center align-center'>
+      <div className='grid grid-cols-2 gap-4'>
+        <button
+          onClick={() => handleRoleSelect("employer")}
+          className='bg-blue-900 col-span-1'
+        >
+          I am an Employer
+        </button>
+        <button
+          onClick={() => handleRoleSelect("candidate")}
+          className='bg-blue-900 col-span-1'
+        >
+          I am a Candidate
+        </button>
+        <button
+          onClick={() => handleLogin()}
+          className='bg-green-900 col-span-2 h-[5rem]'
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
---
+++ Client/tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1040px",
+          "2xl": "1240px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
