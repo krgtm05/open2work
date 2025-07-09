d09503647dcd3141ac825f6f01ad38ec5c4ea356
krgtm05 <krgautamofficial@gmail.com>
2025-07-10 04:16:53 +0530

    fetch my application ui

--- Client/src/Components/Feed/CandidateFeed.jsx
+++ Client/src/Components/Feed/CandidateFeed.jsx
@@ -14,6 +14,8 @@ function CandidateFeed() {
   const [JobsAsideStatus, setJobAsideStatus] = useState(true);
   const [UsersAsideStatus, setUsersAsideStatus] = useState(true);
   const [about, setAbout] = useState(true);
+  const [applications, setApplications] = useState([]);
+  const [myApplicationsStatus, setMyApplicationsStatus] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -27,11 +29,12 @@ function CandidateFeed() {
         setIsLoading(!isLoading);
         setAlljobs(res.data.alljobs);
       } catch (e) {
-        console.error("Failed to fetch jobs:", e);
+        console.log("Failed to fetch jobs:", e);
       }
     }
     setUsersAsideStatus(false);
     setAbout(false);
+    setMyApplicationsStatus(false);
     fetchJobs();
   }, []);
 
@@ -39,6 +42,7 @@ function CandidateFeed() {
     setJobAsideStatus(false);
     setAbout(false);
     setUsersAsideStatus(true);
+    setMyApplicationsStatus(false);
 
     const res = await axios.get("/api/allusers", {
       headers: {
@@ -52,8 +56,24 @@ function CandidateFeed() {
   function fetchAbout() {
     setJobAsideStatus(false);
     setUsersAsideStatus(false);
+    setMyApplicationsStatus(false);
     setAbout(true);
-    console.log("Fetching About...");
+  }
+  async function fetchMyApplications() {
+    try {
+      const res = await axios.get("/api/my-applications", {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      });
+      setApplications(res.data.appliedJobs);
+      setMyApplicationsStatus(true);
+      setJobAsideStatus(false);
+      setUsersAsideStatus(false);
+      setAbout(false);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
@@ -104,6 +124,7 @@ function CandidateFeed() {
                   setJobAsideStatus(true);
                   setUsersAsideStatus(false);
                   setAbout(false);
+                  setMyApplicationsStatus;
                 }}
               >
                 <div className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'>
@@ -117,11 +138,10 @@ function CandidateFeed() {
                     <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
                     <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
                   </svg>
-                  <span className='ms-3'>Dashboard</span>
+                  <span className='ms-3'>Available Jobs</span>
                 </div>
               </li>
-              <li onClick={() => {
-                console.log("Fetching Applied Jobs...");}}>
+              <li onClick={fetchMyApplications}>
                 <div className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'>
                   <svg
                     className='shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
@@ -132,7 +152,9 @@ function CandidateFeed() {
                   >
                     <path d='M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z' />
                   </svg>
-                  <span className='flex-1 ms-3 whitespace-nowrap'>My Applications</span>
+                  <span className='flex-1 ms-3 whitespace-nowrap'>
+                    My Applications
+                  </span>
                 </div>
               </li>
               <li onClick={fetchUsers}>
@@ -173,9 +195,23 @@ function CandidateFeed() {
         <div className='sm:ml-64 px-4 grid grid-cols-1 md:grid-cols-2 gap-4 overflow-y-scroll h-[calc(100vh-10px)] will-change-scroll pb-40 scrollbar-hide'>
           {isLoading && <Spinner />}
           {JobsAsideStatus &&
-            alljobs.map((job) => <JobCard key={job._id} job={job} />)}
+            alljobs.map((job) => (
+              <JobCard key={job._id} job={job} tab={"Jobs"} />
+            ))}
           {UsersAsideStatus &&
             allusers.map((user) => <UserCard key={user._id} user={user} />)}
+          {myApplicationsStatus && applications.length === 0
+            ? "You have not applied to any job yes"
+            : applications
+                .filter((ele) => ele.jobId !== null)
+                .map((ele) => (
+                  <JobCard
+                    key={ele._id}
+                    job={ele.jobId}
+                    tab={"Applications"}
+                    onDelete={fetchMyApplications}
+                  />
+                ))}
           {about && <AboutComp />}
         </div>
       </div>
